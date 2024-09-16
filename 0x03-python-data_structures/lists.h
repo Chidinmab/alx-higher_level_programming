ab302af0875eacb11585fff684640fa14651faf0
Chidinma <chidinmablessingpat@gmail.com>
2024-09-16 08:52:34 -0700

    print list

--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -30,4 +30,10 @@ def delete_at(my_list=[], idx=0):
 int is_palindrome(listint_t **head);
 void print_python_list_info(PyObject *p);
 
+size_t print_listint(const listint_t *h);
+listint_t *add_nodeint_end(listint_t **head, const int n);
+void free_listint(listint_t *head);
+
+int is_palindrome(listint_t **head);
+
 #endif /* LISTS_H */
