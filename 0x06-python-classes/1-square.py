60c37e245032178e7a1fd1cfb63a1d78bb48c9f0
Chidinma <chidinmablessingpat@gmail.com>
2024-10-02 10:28:57 -0700

    1 square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" Define a square class"""
+
+
+class Square:
+    """ rep... of asquare class"""
+
+    def __init__(self, size):
+        """ Initialization with size for our object
+        initialization
+        """
+        self.__size = size
